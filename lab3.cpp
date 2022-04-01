dda9de66d1bd2796c2c580a450b20194d9c32a61
LemeshevOR <LemeshevOR@mpei.ru>
2022-04-01 14:06:46 +0300

    code:SVG histogram

--- lab3.cpp
+++ lab3.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 vector<double> input_numbers(size_t count)
@@ -98,8 +99,53 @@ void show_histogram_text(vector<size_t> bins)
     return;
 }
 
-int main()
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_end() {
+    cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "' >" << text << "</text>";
+}
+
+void show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 1000;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
+    
+}
+
+void svg_rect(double x, double y, double width, double height,string stroke = "black", string fill = "black")
 {
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height
+        << "' stroke='" << stroke << "' fill='" << fill << "' />";
+   
+}
+
+
+
+int main()
+{const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
     // Ввод данных
     size_t number_count;
     cerr << "Enter number count: ";
@@ -115,7 +161,15 @@ int main()
     const auto bins = make_histogram(numbers, bin_count);
 
     // Вывод данных
-    double bin_height;
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
 
+    double top = 0;
+    for (size_t bin : bins) {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"MediumSlateBlue","#E6E6FA");
+        top += BIN_HEIGHT;
+
+    }
+    svg_end();
 }
